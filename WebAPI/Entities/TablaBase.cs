1c41fb5c1579e302b407521552f0628719faa082
mauro_bernal_rios <mauro_bernal_rios@hotmail.com>
2022-03-22 12:24:28 -0300

    Clas inicial

---
+++ WebAPI/Entities/TablaBase.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Entities
+{
+    public abstract class TablaBase
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Habilitado { get; set; }
+    }
+}
