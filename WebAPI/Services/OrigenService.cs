04c03a875d4719751250e403a72aaec6d3cbe107
mauro_bernal_rios <mauro_bernal_rios@hotmail.com>
2022-03-22 12:41:38 -0300

    Servicio Origen

---
+++ WebAPI/Services/OrigenService.cs
@@ -0,0 +1,45 @@
+﻿using WebAPI.Entities;
+namespace WebAPI.Services;
+
+public class Service
+{
+
+    public List<Origen> entidades { get; set; }
+    public int AddOrigen(Origen origen)
+    {
+        origen.Id = new Random().Next(100000);
+        entidades.Add(origen);
+        return origen.Id;
+    }
+
+    public bool UpdateOrigen(Origen origen) {
+
+        var entidad = entidades.Where(o => o.Id == origen.Id ).FirstOrDefault();
+        if (entidad == null) return false;
+
+        entidad.Descripcion = origen.Descripcion;
+        entidad.Habilitado = origen.Habilitado;
+
+        entidades.Add(origen);
+        return true;
+    }
+
+    public bool DeleteOrigen(int ID)
+    {
+        var entidad = entidades.Where(o => o.Id == ID).FirstOrDefault();
+        if (entidad == null) return false;
+        entidades.Remove(entidad);
+        return true;
+
+    }
+
+    public Origen SelectOrigen(int ID) 
+  => entidades.Where(o => o.Id == ID && o.Habilitado == true).FirstOrDefault();
+
+
+    public List<Origen> SelectListOrigen() =>
+        entidades.Where(o => o.Habilitado == true).ToList();
+
+
+
+}
